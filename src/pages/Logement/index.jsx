63f46af48a369ac09f6b1035537f7030a07f0e95
Ilias-Deh <125891153+Ilias-Deh@users.noreply.github.com>
2023-09-08 14:04:17 +0200

    Update index.jsx

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,13 +1,15 @@
 import Carrousel from '../../components/Logement/Carrousel'
 import Description from '../../components/Logement/Description'
-
+import logements from "../../datas/logements.json"
+import { Navigate, useParams} from 'react-router-dom';
 
 function Logement() {
+  const {id} = useParams();
   return (
     <div>
-      <Carrousel />
-      <Description />
+      {logements.find(logement => logement.id === id ) ? <><Carrousel /> <Description /></> :  <Navigate to="/Error" /> }
     </div>
   )
 }
 export default Logement;
+
